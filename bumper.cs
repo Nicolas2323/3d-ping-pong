8c04c68d62ae0b0aceb24870d1ea9bc22303b235
Nicolas2323 <42986955+Nicolas2323@users.noreply.github.com>
2018-11-16 22:52:28 -0500

    update

---
+++ bumper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bumper : MonoBehaviour {
+   public bool isBump1;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (isBump1)
+            transform.Translate(0f, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+        else
+        transform.Translate(0f, Input.GetAxis("Vertical2") * speed * Time.deltaTime, 0f);
+
+
+    }
+}
